e3f8d000ae41a681478c004a375fcf462847fb98
npeuker <nina.peuker@polytechnique.edu>
2023-03-07 12:45:38 +0100

    training for each day and error rates for simple model

--- Salami/functions.py
+++ Salami/functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 import helpFunctions as hf
+import math
+import matplotlib.pyplot as plt
 
 # compute covariance matrix from images
 def covariance(multiIm):
@@ -44,3 +46,199 @@ def means(multiIm,annotationIm):
         mu_meat.append(np.mean(clPix[:,i]))
 
     return mu_fat,mu_meat
+
+# compute parameters
+## returns covariance matrix, inverse of the covariance matrix and factor
+def params(multiIm):
+    cov = covariance(multiIm)
+    det = np.linalg.det(cov)
+    fact = 1/(2*math.pi*math.sqrt(det))
+
+    return cov, np.linalg.inv(cov), fact
+
+
+
+# compute multivariate gaussian in general for mu and parameters
+
+def f(x,mu,params):
+    cov, inv, fact = params
+    mat = np.dot(np.transpose(x-mu),inv)
+    mat = np.dot(mat,(x-mu))
+    return fact*math.exp(-1/2 * mat)
+
+
+def Gauss(x,mu,s):
+    return 1/(s*math.sqrt(2*math.pi)) * np.exp(-1/2 * 1/s**2 * (x-mu)**2)
+
+
+# solve inequality 
+def solveInter(func1,func2,incr,approx):
+    i = 0
+    r = True
+    x = approx
+    result = 0
+    while r:
+        set1 = func1(x)
+        set2 = func2(x)
+        if (set1 - set2) <= 0:
+            r = True
+            i += 1
+        else:
+            r = False
+            result = x
+        x += incr
+    return result
+
+# compute thresholds
+def thresholds(multiIm,mu_meat,mu_fat):
+    bands = len(multiIm[0,0,:])
+    thresholds = []
+    for band in range(bands):
+        s = np.std(multiIm[:,:,band])
+        mu_1 = mu_meat[band]
+        mu_2 = mu_fat[band] 
+
+        def func1(x):
+            return Gauss(x,mu_1,s)
+
+        def func2(x):
+            return Gauss(x,mu_2,s)
+
+        thresholds.append(solveInter(func1,func2,0.001,mu_1))
+    return thresholds
+
+# compute thresholds directly for a day
+def thresholdDay(day, dirIn):
+    # load images
+    multiIm, annotationIm = hf.loadMulti(f'multispectral_day{day}.mat' , f'annotation_day{day}.png', dirIn)
+
+    bands = len(multiIm[0,0,:])
+    thresholds = []
+
+    mu_meat,mu_fat = means(multiIm,annotationIm)
+
+    for band in range(bands):
+        s = np.std(multiIm[:,:,band])
+        mu_1 = mu_meat[band]
+        mu_2 = mu_fat[band] 
+
+        def func1(x):
+            return Gauss(x,mu_1,s)
+
+        def func2(x):
+            return Gauss(x,mu_2,s)
+
+        thresholds.append(solveInter(func1,func2,0.001,mu_1))
+    return thresholds
+
+# function to compute error rates for the simple threshold 
+def errorSimple(multiIm,annotationIm,thresholds):
+    # computing error 
+    # fat pixels
+    ann = 1
+    [clPixFat, rFat, cFat] = hf.getPix(multiIm,annotationIm[:,:,ann])
+    # meat pixels
+    ann = 2
+    [clPixMeat, rMeat, cMeat] = hf.getPix(multiIm,annotationIm[:,:,ann])
+
+    fatPixels = len(rFat)
+    meatPixels = len(rMeat)
+
+    # count classified pixels
+    bandErrorFat = []
+    bandErrorMeat = []
+
+    # the function checkSimple returns 0 for meat and 1 for fat
+    for band in range(len(clPixMeat[0])):
+        t = thresholds[band]
+        fatsClassification = 0
+        for i in range(fatPixels):
+            f = multiIm[rFat[i],cFat[i],band]
+            fatsClassification += (f>t)
+        meatClassification = 0
+        for i in range(meatPixels):
+            m = multiIm[rMeat[i],cMeat[i],band]
+            meatClassification += (m<t)
+        bandErrorFat.append(1-fatsClassification/fatPixels)
+        bandErrorMeat.append(1-meatClassification/meatPixels)
+
+    return bandErrorFat,bandErrorMeat
+
+# determine the spectral band with the best discriminative properties for meat and fat
+def findBestBand(bandErrorFat,bandErrorMeat):
+    dist = np.sqrt(np.array(bandErrorMeat)**2 + np.array(bandErrorFat)**2)
+    ind = np.where(dist == min(dist))[0][0]
+    return ind
+
+# show the best image for simple threshold method
+
+def showClassificationSimple(multiIm,annotationIm,bandErrorFat,bandErrorMeat,thresholds):
+    # classify image for salami on day 1 - band 2
+    ind = findBestBand(bandErrorFat,bandErrorMeat)
+    image = multiIm[:,:,ind]
+
+    background = annotationIm[:,:,0]
+    fatLayer = np.zeros(image.shape)
+    meatLayer = np.zeros(image.shape)
+
+    x,y = image.shape
+
+    t = thresholds[ind]
+
+    colours = [[[] for i in range(x)] for j in range(y)]
+    blue = [0, 0, 255] 
+    green = [0, 255, 0]
+    red = [255, 0, 0]
+
+
+
+    for i in range(x):
+        for j in range(y):
+            fatLayer[i][j] = (multiIm[i,j,ind] > t)
+            meatLayer[i][j] = multiIm[i,j,ind] < t
+            if(not background[i][j]):
+                colours[i][j] = blue
+            elif(fatLayer[i][j]):
+                # fat
+                colours[i][j] = green
+            else:
+                # meat
+                colours[i][j] = red
+
+    plt.imshow(colours)
+    plt.title("Colour annotation")
+    plt.show()
+
+
+
+# compute simple error rates for day x
+def errorRatesDay(day,dirIn,t):
+    # load images
+    multiIm, annotationIm = hf.loadMulti(f'multispectral_day{day}.mat' , f'annotation_day{day}.png', dirIn)
+
+    # compute error rates
+    bandErrorFat,bandErrorMeat = errorSimple(multiIm,annotationIm,t)
+
+    # determine best spectral band
+    ind = findBestBand(bandErrorFat,bandErrorMeat)
+
+    print(f"best spectral band is {ind}")
+
+    # return error rates of that band
+    return (bandErrorFat[ind]+bandErrorMeat[ind])/2
+
+def errorRates(training,dirIn):
+    t = thresholdDay(training,dirIn)
+    days = ['01','06','13','20','28']
+    days.remove(training)
+    print(days)
+
+    errorRates = []
+    for day in days:
+        error = errorRatesDay(day,dirIn,t)
+        print(day,error)
+        errorRates.append(error)
+    
+    return errorRates
+
+
